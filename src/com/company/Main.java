de7de0edb3ced00fca64097b68c2b3e252d098bf
LuliaVert <lulia.vert@gmail.com>
2017-12-09 21:29:34 +0200

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            try {
+                // Create new file
+                String content = "This is the content to write into create file";
+                String path = "/Users/LuliaVert/Desktop/FileCreate/hello.txt";
+                File file = new File(path);
+
+
+                // If file doesn't exists, then create it
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+
+                FileWriter fw = new FileWriter(file.getAbsoluteFile());
+                BufferedWriter bw = new BufferedWriter(fw);
+
+                // Write in file
+                bw.write(content);
+
+                // Close connection
+                bw.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+
+    }
